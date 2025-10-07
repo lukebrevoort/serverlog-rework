dc3286a185e53f91e914f0cdf3c9c2cfb503a642
lukebrevoort <luke@brevoort.com>
2025-10-07 16:47:49 -0400

    Add Docker configuration and Nginx setup for web service; update database connection settings

--- server/src/database.py
+++ server/src/database.py
@@ -1,18 +1,16 @@
 import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://secureloguser:securelogpass@localhost:5432/securelogdb",
+    "postgresql://secureloguser:securelogpass@localhost:5432/securelogdb"
 )
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
