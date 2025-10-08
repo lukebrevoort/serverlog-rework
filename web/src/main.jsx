515464d378e6f32d018a99037a48ab8e51b91ce4
lukebrevoort <luke@brevoort.com>
2025-10-08 13:51:24 -0400

    feat: add theme toggle functionality and implement ThemeProvider for theme management

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from './components/ThemeProvider.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+      <ThemeProvider defaultTheme='system' storageKey='securelog-theme'>
+        <App />
+      </ThemeProvider>
+    </StrictMode>
 )
